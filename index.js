fc49dd5ca1537729568a9a00404ee541a4a0550c
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-13 15:30:16 -0500

    commit tree 4fde9c1d43cbe73649255bee45a2c023ef51e6a7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector('main')
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "YOUR-NAME is the champion"
+
+const body = document.querySelector('body')
+
+
+body.append(newHeader)
